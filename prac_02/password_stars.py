387c1acd587ab65323699aff7845b3dd34effd6a
XxxiaoyuGIT <xiaoyu.shi@my.jcu.edu.au>
2023-09-21 16:48:43 +1000

    prac02 work

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,8 @@
 def main():
-    score=input("Number:")
-    print(star(score))
-def star(score):
-    return "*" * int(score)
+    s=input("Number:")
+    print(star(s))
+def star(s):
+    return "*" * int(s)
 
 if __name__ == "__main__":
     main()
